4a371a9ff3dd08869336d2a9317919061002f6d8
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-06-10 16:29:57 +0900

    feat: 예외 핸들러 구현

---
+++ src/main/java/finalmission/global/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package finalmission.global;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public String illegalArgumentException(Exception e) {
+        return "서버 에러입니다.";
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String illegalArgumentException(IllegalArgumentException e) {
+        return e.getMessage();
+    }
+}
